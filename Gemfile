9bac5e72e30233b9404932720c4b27935e897d88
kijima <norimaking777@gmail.com>
2021-11-23 15:28:50 +0900

    Add: Working

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'csv'
-gem 'open-uri'
+gem 'httpclient'
 gem 'rspec'
 gem 'simplecov'
-gem 'nokogiri'
--- src/main.rb
+++ src/main.rb
@@ -1,40 +1,76 @@
 # frozen_string_literal: true
 
 require_relative 'dsl'
-require 'nokogiri'
-require 'open-uri'
+require 'httpclient'
+require 'json'
 
-dsl = GemfileExporter::Dsl.new
+def main
 
-p dsl.dependencies.map(&:name)
+  dsl = GemfileExporter::Dsl.new
 
-base_url = "#{dsl.sources.all_sources[0].remotes[0].to_s}gems/"
+  # p dsl.dependencies.map(&:name)
 
-dsl.dependencies.each do |gem|
-  sleep 0.1
-  p "start #{gem.name}..."
-  url = base_url + gem.name
-  web = URI.open(url)
+  base_url = "#{dsl.sources.all_sources[0].remotes[0].to_s}gems/"
 
-  doc = Nokogiri::HTML.parse(web.read)
-  release_url = doc.xpath('//*[@id="code"]')[0][:href]
+  dsl.dependencies.each do |gem|
+    # return unless gem == dsl.dependencies.first
+    sleep 0.1
+    p "start #{gem.name}..."
 
-  # "https://github.com/rails/rails/tree/v6.1.4.1"
+    url = "https://rubygems.org/api/v1/gems/#{gem.name}.json"
 
-  reg = %r{https://github.com/(\w+)/(\w+)}
-  m = reg.match(release_url)
-  user = m[1]
-  repo = m[2]
+    client = HTTPClient.new
+    request =  client.get(url)
+    response = JSON.parse(request.body)
+    puts JSON.pretty_generate(response)
 
-  gh_url = "https://github.com/#{user}/#{repo}"
-  p gh_url
+    reg = %r{https://github.com/([\w\-]+)/([\w\-+])}
+    github_gem = github_url(response.dig('metadata', 'homepage_uri')) ||
+                 github_url(response.dig('homepage_uri')) ||
+                 github_url(response.dig('bug_tracker_uri')) ||
+                 github_url(response.dig('source_code_uri'))
 
-  # p dsl.git_sources
-  # p dsl.groups
-  # p dsl.gemspecs
+    # reg.match(h.to_s)
 
-  # rubygemsのURLをどうやって生成しているのか
-  # 実際どこでダウンロードしてるのかわからない
-  # githubのURLを取るにはスクレイピングしかなさそう
+    # if github_gems[0].class == Hash
+    #   github_gems = github_gems[0].values
+    # end
+    p github_gem
+    m = github_gem
+    user = m[1]
+    repo = m[2]
+    p gh_url = "https://github.com/#{user}/#{repo}"
 
+    # p response['source_code_uri']
+
+
+    # doc = Nokogiri::HTML.parse(web.read)
+    # release_url = doc.xpath('//*[@id="code"]')[0][:href]
+
+    # # "https://github.com/rails/rails/tree/v6.1.4.1"
+
+    # reg = %r{https://github.com/(\w+)/(\w+)}
+    # m = reg.match(release_url)
+    # user = m[1]
+    # repo = m[2]
+
+    # gh_url = "https://github.com/#{user}/#{repo}"
+    # p gh_url
+
+    # p dsl.git_sources
+    # p dsl.groups
+    # p dsl.gemspecs
+
+    # rubygemsのURLをどうやって生成しているのか
+    # 実際どこでダウンロードしてるのかわからない
+    # githubのURLを取るにはスクレイピングしかなさそう
+
+  end
 end
+
+def github_url (url)
+  reg = %r{https://github.com/([\w\-]+)/([\w\-+])}
+  reg.match(url)
+end
+
+main()
