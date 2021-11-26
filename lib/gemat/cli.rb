807d6d227ffbc5f8dc34a357f5f1624edad9c132
kijima <norimaking777@gmail.com>
2021-11-26 22:51:28 +0900

    Refactor: remove redundunt code

--- lib/gemat/cli.rb
+++ lib/gemat/cli.rb
@@ -6,18 +6,19 @@ module Gemat
   class Cli < Thor
     desc 'csv', 'export Gemfile to CSV file'
     def csv
-      dsl = Gemat::Dsl.new
-      url = Gemat::GetUrl.new(dsl)
-      url.run
-      Gemat::CsvFormatter.new(url).to_csv
+      run { |url| Gemat::CsvFormatter.new(url).to_csv }
     end
 
     desc 'md', 'export Gemfile to markdown'
     def md
+      run { |url| Gemat::MdFormatter.new(url).to_md }
+    end
+
+    def run(&block)
       dsl = Gemat::Dsl.new
       url = Gemat::GetUrl.new(dsl)
       url.run
-      Gemat::MdFormatter.new(url).to_md
+      block.call(url)
     end
   end
 end
