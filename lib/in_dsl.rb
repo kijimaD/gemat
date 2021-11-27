0b5ea8b52acc9c04972f77f67fa6c3f05d4ed00d
kijima <norimaking777@gmail.com>
2021-11-27 16:19:20 +0900

    Add: gemfiles accessor

--- lib/in_dsl.rb
+++ lib/in_dsl.rb
@@ -4,21 +4,24 @@ require 'bundler'
 
 module Gemat
   class InDsl
-    attr_accessor :dependencies, :sources, :git_sources, :groups
+    attr_accessor :dependencies, :sources, :git_sources, :groups, :gemfiles
 
+    # rubocop:disable Metrics/MethodLength
     def initialize(gemfile = nil)
       gemfile ||= Bundler.default_gemfile.to_s
       dsl = Bundler::Dsl.new
       dsl.eval_gemfile(gemfile)
 
       class << dsl
-        attr_accessor :sources, :git_sources, :groups
+        attr_accessor :sources, :git_sources, :groups, :gemfiles
       end
 
       @dependencies = dsl.dependencies
       @sources = dsl.sources
       @git_sources = dsl.git_sources
       @groups = dsl.groups
+      @gemfiles = dsl.gemfiles
     end
+    # rubocop:enable Metrics/MethodLength
   end
 end
