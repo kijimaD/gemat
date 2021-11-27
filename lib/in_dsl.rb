d433ef928588f32dff6b5cf39c393e1e16f53252
kijima <norimaking777@gmail.com>
2021-11-27 11:48:14 +0900

    Lint: Lint/RedundantCopDisableDirective

--- lib/in_dsl.rb
+++ lib/in_dsl.rb
@@ -6,7 +6,6 @@ module Gemat
   class InDsl
     attr_accessor :dependencies, :sources, :git_sources, :groups
 
-    # rubocop:disable Metrics/MethodLength
     def initialize(gemfile = nil)
       gemfile ||= Bundler.default_gemfile.to_s
       dsl = Bundler::Dsl.new
@@ -21,6 +20,5 @@ module Gemat
       @git_sources = dsl.git_sources
       @groups = dsl.groups
     end
-    # rubocop:enable Metrics/MethodLength
   end
 end
